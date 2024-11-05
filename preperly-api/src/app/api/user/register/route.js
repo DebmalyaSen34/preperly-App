0dd6631dc50be062e83846692a66186ba22f928f
DebmalyaSen34 <ds22btb0a15@student.nitw.ac.in>
2024-11-05 22:30:24 +0530

    Added registration API

--- preperly-api/src/app/api/user/register/route.js
+++ preperly-api/src/app/api/user/register/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import User from "@/models/user";
 import connectToDatabase from "@/utils/db";
 
-export async function GET(request){
+export async function POST(request){
     try {
         const {
             restaurantName,
@@ -17,12 +17,36 @@ export async function GET(request){
             receiveUpdatesOnWhatsApp
         } = request.body;
 
-        connectToDatabase();
+        if(!restaurantName || !restaurantAddress || !phoneNumber || !email || !password || !ownerName || !ownerPhoneNumber || !ownerEmail){
+            return NextResponse.json({message: "Please fill in all the fields!"}, {status: 400});
+        }
+
+        await connectToDatabase();
+
+        const existingUser = await User.findOne({phoneNumber});
+
+        if(existingUser){
+            return NextResponse.json({message: "User already exists!"}, {status: 400});
+        }
+
+        const user = new User({
+            restaurantName,
+            restaurantAddress,
+            phoneNumber,
+            alternateNumber,
+            email,
+            password,
+            ownerName,
+            ownerPhoneNumber,
+            ownerEmail,
+            receiveUpdatesOnWhatsApp
+        });
+
+        await user.save();
 
         return NextResponse.json({message: "User is successfully registered!"}, {status: 200});
     } catch (error) {
-        return NextResponse.json({message: "Error"}, {status: 500});
+        return NextResponse.json({message: "There was en error while registering you!"}, {status: 500});
         
     }
-    return NextResponse.json({message: "Hello World"}, {status: 200});
 }
